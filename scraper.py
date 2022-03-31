b36522c71b4b6e4ee846804a50064ff60264dfbe
Lihao Deng <lihao-deng@outlook.com>
2022-03-31 10:01:51 +0800

    1

--- scraper.py
+++ scraper.py
@@ -149,7 +149,8 @@ def wd_login(xuhao, mima):
 
         except Exception as e:
             print(e)
-            print(f"第{i+1}次运行失败！")
+            print(f"第{str(i+1)}次运行失败！")
+
             time.sleep(10)
 
 
